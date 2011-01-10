a1c876a8892244bf78a56c861d0f2ad536cf8c00
opprecht <opprecht@localhost>
2011-01-10 17:40:46 +0100

    use $CFG for directory permissions

--- lib/ims/lib/file_util.class.php
+++ lib/ims/lib/file_util.class.php
@@ -9,36 +9,31 @@
  * @author laurent.opprecht@unige.ch
  *
  */
-class FileUtil{
+class FileUtil {
 
-  	public static function ensure_directory($path){
-  		$path = rtrim($path, '/');
-  		$is_dir = strpos(basename($path), '.') === false;
-  		$dir = $is_dir ? $path : dirname($path);
-  		if(!file_exists($dir)){
-  			$result = mkdir($dir, '0777', true);
-  			return $result;
-  		}else{
-  			return true;
-  		}
-  	}
+    public static function ensure_directory($path) {
+        global $CFG;
+        $path = rtrim($path, '/');
+        $is_dir = strpos(basename($path), '.') === false;
+        $dir = $is_dir ? $path : dirname($path);
+        if (!file_exists($dir)) {
+            $result = mkdir($dir, $CFG->directorypermissions, true);
+            return $result;
+        } else {
+            return true;
+        }
+    }
 
-    public function write($path, $content){
-    	if (!$fh=fopen($path, 'w')) {
-        	$result = false;
-        }else{
-        	$result = (bool)fwrite($fh, $content);
-		}
+    public function write($path, $content) {
+        if (!$fh = fopen($path, 'w')) {
+            $result = false;
+        } else {
+            $result = (bool) fwrite($fh, $content);
+        }
         fclose($fh);
         return $result;
     }
 
 }
-
-
-
-
-
-
 
 ?>
